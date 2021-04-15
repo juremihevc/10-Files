50cbeae93337256e0e91fa13a0af5b2d67019d50
Jure Mihevc <juremihevcster@gmail.com>
2021-04-15 20:34:37 +0200

    create Scorring_game2.py and 11 Lists and Dictionaries.py

---
+++ 11 Lists and Dictionaries.py
@@ -0,0 +1,15 @@
+some_list = [1, 34, 12, 17, 87]
+some_list.sort()
+print(some_list)
+
+# for number in some_list:
+#     print(number)
+#
+# another_list = ["tesla", "toyota", "nissan"]
+# print(another_list)
+#
+# mixed_list = [22, "elon", True, "SmartNinja", 3.14]
+# print(mixed_list)
+
+# for element in mixed_list:
+#     print(element)
